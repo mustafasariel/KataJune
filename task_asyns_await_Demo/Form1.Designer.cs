5ca73979f735ef9b4abe526c6ebc3143809ab16a
Mustafa SARIEL <mustafasariel@gmail.com>
2020-06-14 10:00:42 +0300

    Add project files.

---
+++ task_asyns_await_Demo/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace task_asyns_await_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCounter = new System.Windows.Forms.Button();
+            this.txtCounter = new System.Windows.Forms.TextBox();
+            this.btnReadAsync = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 12);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(111, 49);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 67);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(370, 303);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCounter
+            // 
+            this.btnCounter.Location = new System.Drawing.Point(271, 12);
+            this.btnCounter.Name = "btnCounter";
+            this.btnCounter.Size = new System.Drawing.Size(111, 49);
+            this.btnCounter.TabIndex = 2;
+            this.btnCounter.Text = "Counter";
+            this.btnCounter.UseVisualStyleBackColor = true;
+            this.btnCounter.Click += new System.EventHandler(this.btnCounter_Click);
+            // 
+            // txtCounter
+            // 
+            this.txtCounter.Location = new System.Drawing.Point(388, 12);
+            this.txtCounter.Multiline = true;
+            this.txtCounter.Name = "txtCounter";
+            this.txtCounter.Size = new System.Drawing.Size(161, 49);
+            this.txtCounter.TabIndex = 3;
+            // 
+            // btnReadAsync
+            // 
+            this.btnReadAsync.Location = new System.Drawing.Point(129, 12);
+            this.btnReadAsync.Name = "btnReadAsync";
+            this.btnReadAsync.Size = new System.Drawing.Size(111, 49);
+            this.btnReadAsync.TabIndex = 4;
+            this.btnReadAsync.Text = "Read async";
+            this.btnReadAsync.UseVisualStyleBackColor = true;
+            this.btnReadAsync.Click += new System.EventHandler(this.btnReadAsync_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(388, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(161, 303);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 382);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnReadAsync);
+            this.Controls.Add(this.txtCounter);
+            this.Controls.Add(this.btnCounter);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCounter;
+        private System.Windows.Forms.TextBox txtCounter;
+        private System.Windows.Forms.Button btnReadAsync;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ task_asyns_await_Demo/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace task_asyns_await_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string str = string.Empty;
+            richTextBox1.Text = GetRead();
+
+        }
+        private async void btnReadAsync_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.Text =await GetReadAsync();
+        }
+        string GetRead()
+        {
+            using (StreamReader streamReader = new StreamReader("readme.txt"))
+            {
+                var s = streamReader.ReadToEnd();
+                Thread.Sleep(5000);
+               // Task.Delay(5000);
+                return s;
+            }
+        }
+        int counter = 0;
+        private void btnCounter_Click(object sender, EventArgs e)
+        {
+            txtCounter.Text = counter++.ToString();
+        }
+
+        async Task<string> GetReadAsync()
+        {
+            using (StreamReader streamReader = new StreamReader("readme.txt"))
+            {
+                Task<string> mytask = streamReader.ReadToEndAsync();
+               
+                for (int i = 0; i < 100; i++)
+                {
+                    listBox1.Items.Insert(0, i);
+                    await Task.Delay(50);
+                    //Thread.Sleep(50);
+                }
+                await Task.Delay(500);
+                return await mytask;
+            }
+        }
+
+        
+    }
+}
